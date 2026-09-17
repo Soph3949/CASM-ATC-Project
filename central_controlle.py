d9391022fd1e5ebc6f4b9ea629e838cab4228f65
Soph3949 <sophia.jch473@gmail.com>
2026-09-17 13:23:18 -0400

    Fix user login validation error
    s

---
+++ central_controlle.py
@@ -0,0 +1,24 @@
+drone_1_uri = "radio://0/80/2M/E7E7E7E701"
+drone_2_uri = "radio://0/80/2M/E7E7E7E702"
+safety_radius = 0.40
+low_altitute_layer = 0.60
+high_altitute_layer = 0.90
+live_airspace_registry = {}
+
+def get_node_coordinates(landmark_name):
+    landmark = landmark_name.lower().strip()
+    if landmark = "parliament":
+        return( , , )
+    if landmark = "kanata":
+        return( , , )
+    if landmark = "orleans":
+        return( , , )
+    if landmark = "barrhaven":
+        return( , , )
+    if landmark = "the glebe":
+        return( , , )
+    if landmark = "westboro":
+        return( , , )
+    if landmark = "nepean":
+        return( , , )
+    
